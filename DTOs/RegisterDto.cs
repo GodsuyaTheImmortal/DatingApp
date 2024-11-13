f88bd734beef24eba5950da4ae2f4ba90b1a7eea
GodsuyaTheImmortal <lawtq-wp20@student.tarc.edu.my>
2024-11-13 18:13:57 +0800

    End of Section 11

---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string  Username { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+}
